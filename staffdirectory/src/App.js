ce8b0c31c927cb4e80497cb33d4ba53011b391e0
pinghsu520 <pinghsu520@gmail.com>
2020-09-09 13:58:47 -0700

    initial

---
+++ staffdirectory/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Graph from "./Graph.js";
+import Staff from './staff.jsx'
+function App() {
+  return (
+    <div className="App">
+      <Staff/>
+    </div>
+  );
+}
+
+export default App;
